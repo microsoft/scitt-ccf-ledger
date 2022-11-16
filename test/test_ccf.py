1cd6f630ff85faf25316e7ca95a6b96081328f3a
Ryan Zhang <112638134+ryazhang-microsoft@users.noreply.github.com>
2022-11-16 10:57:00 -0500

    Update test/test_ccf.py
    Co-authored-by: Maik Riechert <maik.riechert@microsoft.com>

--- test/test_ccf.py
+++ test/test_ccf.py
@@ -162,7 +162,7 @@ def test_accepted_did_issuers(tmp_path: Path):
         not_allowed(lambda: submit(alg="ES256", kty="ec", ec_curve="P-256"))
 
         # Add just one issuer to the policy. Claims signed with this
-        # issuers are accepted.
+        # issuer are accepted.
         identity = fixture.did_web_server.create_identity(alg="ES256", kty="ec", ec_curve="P-256")
         claims = crypto.sign_json_claimset(identity, {"foo": "bar"})
         fixture.configure_service({"policy": {"accepted_did_issuers": [identity.issuer]}})
