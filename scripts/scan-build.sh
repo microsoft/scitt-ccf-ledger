cc733379f0941ff74293cfcf2631b4098253f873
ryazhang-microsoft <ryazhang@microsoft.com>
2022-11-23 09:53:46 -0500

    clean up

--- scripts/scan-build.sh
+++ scripts/scan-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the Apache 2.0 License.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 set -ex
 
