9f0a21d2b1a9316c32ac69d2b888557c53e13582
ryazhang-microsoft <ryazhang@microsoft.com>
2022-11-30 08:51:24 -0500

    clean up

--- scripts/scan-build.sh
+++ scripts/scan-build.sh
@@ -4,4 +4,4 @@
 
 CLANG_VERSION=10
 
-ENCLAVE_TYPE=virtual CMAKE_BUILD_TYPE=Debug BUILD_DIR=scan-build scan-build-$CLANG_VERSION --use-cc=clang-$CLANG_VERSION --use-c++=clang++-$CLANG_VERSION --exclude 3rdparty --exclude test --exclude unit-tests --status-bugs ./build.sh
+BUILD_TESTS=OFF ENCLAVE_TYPE=virtual CMAKE_BUILD_TYPE=Debug BUILD_DIR=scan-build scan-build-$CLANG_VERSION --use-cc=clang-$CLANG_VERSION --use-c++=clang++-$CLANG_VERSION --exclude 3rdparty --status-bugs ./build.sh
