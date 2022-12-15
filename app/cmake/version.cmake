d763530d5b6b6383ed508d769927a2ace6959f2c
Maik Riechert <maik.riechert@microsoft.com>
2022-12-15 13:59:25 +0000

    forward version to docker build

--- app/cmake/version.cmake
+++ app/cmake/version.cmake
@@ -2,21 +2,23 @@
 # Licensed under the MIT License.
 
 unset(SCITT_VERSION)
-unset(SCITT_RELEASE_VERSION)
-unset(SCITT_VERSION_SUFFIX)
 
-# Deduce project version from git environment
-find_package(Git)
+if (SCITT_VERSION_OVERRIDE)
+  set(SCITT_VERSION "${SCITT_VERSION_OVERRIDE}")
+else()
+  # Deduce project version from git environment
+  find_package(Git)
 
-execute_process(
-  COMMAND "bash" "-c" "${GIT_EXECUTABLE} describe --tags --match=\"*.*.*\""
-  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-  OUTPUT_VARIABLE "SCITT_VERSION"
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  RESULT_VARIABLE RETURN_CODE
-)
-if(NOT RETURN_CODE STREQUAL "0")
-  message(FATAL_ERROR "Error calling git describe")
+  execute_process(
+    COMMAND "bash" "-c" "${GIT_EXECUTABLE} describe --tags --match=\"*.*.*\""
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    OUTPUT_VARIABLE "SCITT_VERSION"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    RESULT_VARIABLE RETURN_CODE
+  )
+  if(NOT RETURN_CODE STREQUAL "0")
+    message(FATAL_ERROR "Error calling git describe")
+  endif()
 endif()
 
 file(WRITE ${CMAKE_BINARY_DIR}/VERSION "${SCITT_VERSION}")
