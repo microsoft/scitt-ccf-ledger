0dabf2cfb5c8a6558ed3adbe7e681a047615ef19
ryazhang-microsoft <ryazhang@microsoft.com>
2022-12-14 23:22:19 -0500

    test without header analuze

--- scripts/scan-build.sh
+++ scripts/scan-build.sh
@@ -4,4 +4,4 @@
 
 CLANG_VERSION=10
 
-BUILD_TESTS=OFF PLATFORM=virtual CMAKE_BUILD_TYPE=Debug BUILD_DIR=scan-build scan-build-$CLANG_VERSION -analyze-headers --use-cc=clang-$CLANG_VERSION --use-c++=clang++-$CLANG_VERSION --exclude 3rdparty --status-bugs ./build.sh
+BUILD_TESTS=OFF PLATFORM=virtual CMAKE_BUILD_TYPE=Debug BUILD_DIR=scan-build scan-build-$CLANG_VERSION --use-cc=clang-$CLANG_VERSION --use-c++=clang++-$CLANG_VERSION --exclude 3rdparty --status-bugs ./build.sh
