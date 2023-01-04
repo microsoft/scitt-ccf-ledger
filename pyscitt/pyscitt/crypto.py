c286c16d91eca3f118471e391771a634218578df
Maik Riechert <maik.riechert@microsoft.com>
2023-01-04 15:05:53 +0000

    formatting

--- pyscitt/pyscitt/crypto.py
+++ pyscitt/pyscitt/crypto.py
@@ -639,7 +639,7 @@ def create_did_document(
     return {
         "@context": [
             "https://www.w3.org/ns/did/v1",
-            "https://w3id.org/security/suites/jws-2020/v1"
+            "https://w3id.org/security/suites/jws-2020/v1",
         ],
         "id": did,
         "assertionMethod": [
