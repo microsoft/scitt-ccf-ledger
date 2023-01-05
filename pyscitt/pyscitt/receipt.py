3323fc4bace2be3e62e30a75e51814f7009e4285
Paul Lietar <lietarpaul@microsoft.com>
2023-01-05 17:29:18 +0000

    Change the signature in receipts from ASN1/DER to IEEE encoding.
    So far, we has been encoding the ECDSA signatures in receipts using the
    ASN1/DER encoding that is commonly found in the x509 ecosystem, and
    which OpenSSL and CCF use. This encoding prefixes each of the two
    components with a tag and length, and groups the two in a SEQUENCE.
    
    By contrast, the IEEE encoding is used by the COSE and JOSE ecosystems
    already. It is merely the two integer values padded to the curve size
    and concatenated together.
    
    This is a breaking change, as old receipts cannot be verified by newer
    clients, and vice-versa. As the signature encoding happens on-demand
    when receipts are requested, if existing ledger is updated all receipts
    it issues going forward will use the new format, including for claims
    that were registered prior to the upgrade.
    
    Fixes #49

--- pyscitt/pyscitt/receipt.py
+++ pyscitt/pyscitt/receipt.py
@@ -5,12 +5,13 @@ import base64
 import hashlib
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Tuple
 
 import cbor2
 import ccf.receipt
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import encode_dss_signature
 from cryptography.x509 import load_der_x509_certificate
 from pycose.messages import Sign1Message
 from pycose.messages.cosebase import CoseBase
@@ -102,9 +103,12 @@ class CCFReceiptContents(ReceiptContents):
         claims_digest = hashlib.sha256(tbs).digest()
 
         root = self.root(claims_digest).hex()
-        signature = base64.b64encode(self.signature).decode()
 
-        ccf.receipt.verify(root, signature, node_cert)
+        # The CCF module expects a base64 signature, in ASN1/DER format.
+        signature = convert_p1363_signature_to_dss(self.signature)
+        b64signature = base64.b64encode(signature).decode()
+
+        ccf.receipt.verify(root, b64signature, node_cert)
         ccf.receipt.check_endorsement(node_cert, service_cert)
 
     def as_dict(self) -> dict:
@@ -173,3 +177,33 @@ class Receipt:
             "protected": hdr_as_dict(self.phdr),
             "contents": self.contents.as_dict(),
         }
+
+
+def decode_p1363_signature(signature: bytes) -> Tuple[int, int]:
+    """
+    Decode an ECDSA signature from its IEEE P1363 encoding into its r and s
+    components. The two integers are padded to the curve size and concatenated.
+
+    This is the format used throughout the COSE/JOSE ecosystem.
+    """
+    # The two components are padded to the same size, so we can find the size
+    # of each one by taking half the size of the signature.
+    assert len(signature) % 2 == 0
+    mid = len(signature) // 2
+    r = int.from_bytes(signature[:mid], "big")
+    s = int.from_bytes(signature[mid:], "big")
+    return r, s
+
+
+def convert_p1363_signature_to_dss(signature: bytes) -> bytes:
+    """
+    Convert an ECDSA signature from its IEEE P1363 encoding to an ASN1/DER
+    encoding.
+
+    The former is the format used throughout the COSE/JOSE ecosystem. The
+    latter is used by OpenSSL and cryptography, as well as the CCF python
+    module.
+
+    """
+    r, s = decode_p1363_signature(signature)
+    return encode_dss_signature(r, s)
