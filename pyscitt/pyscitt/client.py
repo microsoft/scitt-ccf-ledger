430918855c79eb788f6c8bb762b90d97904c0185
Maik Riechert <maik.riechert@microsoft.com>
2023-01-17 09:34:44 +0000

    add test
    Signed-off-by: Maik Riechert <maik.riechert@microsoft.com>

--- pyscitt/pyscitt/client.py
+++ pyscitt/pyscitt/client.py
@@ -59,6 +59,7 @@ class HttpSig(httpx.Auth):
 
 @dataclass
 class ServiceError(Exception):
+    headers: dict
     code: str
     message: str
 
@@ -265,7 +266,7 @@ class BaseClient:
 
         if not response.is_success:
             error = response.json()["error"]
-            raise ServiceError(error["code"], error["message"])
+            raise ServiceError(response.headers, error["code"], error["message"])
 
         if wait_for_confirmation:
             self.wait_for_confirmation(response.headers[CCF_TX_ID_HEADER])
---
+++ test/test_tracing.py
@@ -0,0 +1,27 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import re
+
+import pytest
+
+from pyscitt.client import Client, ServiceError
+
+REQUEST_ID_HEADER = "x-ms-request-id"
+CLIENT_REQUEST_ID_HEADER = "x-ms-client-request-id"
+REQUEST_ID_REGEX = re.compile(r"[0-9a-f]+")
+
+def test_tracing_headers(client: Client):
+    response = client.get("/version")
+    assert CLIENT_REQUEST_ID_HEADER not in response.headers
+    assert REQUEST_ID_REGEX.match(response.headers[REQUEST_ID_HEADER])
+
+    response = client.get("/version", headers={CLIENT_REQUEST_ID_HEADER: "123"})
+    assert response.headers[CLIENT_REQUEST_ID_HEADER] == "123"
+    assert REQUEST_ID_REGEX.match(response.headers[REQUEST_ID_HEADER])
+
+    with pytest.raises(ServiceError, match="InvalidInput") as exc_info:
+        client.get("/version", headers={CLIENT_REQUEST_ID_HEADER: "123 456"})
+    error = exc_info.value
+    assert CLIENT_REQUEST_ID_HEADER not in error.headers
+    assert REQUEST_ID_REGEX.match(error.headers[REQUEST_ID_HEADER])
