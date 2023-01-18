e31560bf07c6e0e5bd389fe7ec97c1a396e6d857
ryazhang-microsoft <ryazhang@microsoft.com>
2023-01-18 15:51:05 -0500

    clean up

--- pyscitt/pyscitt/key_vault_sign_client.py
+++ pyscitt/pyscitt/key_vault_sign_client.py
@@ -7,15 +7,19 @@ import hashlib
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.certificates import CertificateClient, KeyVaultCertificate
 from azure.keyvault.keys import KeyClient
-from azure.keyvault.keys.crypto import CryptographyClient
+from azure.keyvault.keys.crypto import CryptographyClient, SignatureAlgorithm
 from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePublicKey
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 from cryptography.x509 import load_pem_x509_certificate
 
 from pyscitt.client import MemberAuthenticationMethod
 
 from . import crypto
+
+ALGORITHMS = {
+    256: ("ES256", "sha256"),
+    384: ("ES384", "sha384"),
+    521: ("ES512", "sha384"),
+}
 
 
 class KeyVaultSignClient(MemberAuthenticationMethod):
@@ -41,7 +45,7 @@ class KeyVaultSignClient(MemberAuthenticationMethod):
 
     @staticmethod
     def _encode_certificate(cert: KeyVaultCertificate) -> str:
-        assert isinstance(cert.cer, bytes)
+        assert isinstance(cert.cer, bytearray)
         decoded = base64.b64encode(cert.cer).decode()
         cert_pem = f"-----BEGIN CERTIFICATE-----\n{decoded}\n-----END CERTIFICATE-----"
         return cert_pem
@@ -54,12 +58,13 @@ class KeyVaultSignClient(MemberAuthenticationMethod):
         )
         crypto_client = CryptographyClient(key, credential=self.credential)
         cert = load_pem_x509_certificate(self.cert.encode("ascii"))
-        assert isinstance(cert.public_key, (EllipticCurvePublicKey))
-        key_size = cert.public_key().curve.key_size
-        signature_algorithm = "ES" + str(key_size)
-        hash_algorithm = "sha" + str(key_size)
+        pub_key = cert.public_key()
+        assert isinstance(pub_key, (EllipticCurvePublicKey))
+        key_size = pub_key.curve.key_size
+        signature_algorithm, hash_algorithm = ALGORITHMS[key_size]
+
         digest_to_sign = hashlib.new(hash_algorithm, data).digest()
         sign_result = crypto_client.sign(
-            algorithm=signature_algorithm, digest=digest_to_sign
+            algorithm=SignatureAlgorithm(signature_algorithm), digest=digest_to_sign
         )
         return crypto.convert_p1363_signature_to_dss(sign_result.signature)
