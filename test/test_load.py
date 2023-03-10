8994a58ad74ae710aa6f95807dd3e577fd7d3688
Maik Riechert <maik.riechert@microsoft.com>
2023-03-10 10:30:22 +0000

    formatting

--- test/test_load.py
+++ test/test_load.py
@@ -1,9 +1,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+import json
 import subprocess
 from pathlib import Path
-import json
 
 import pytest
 
@@ -19,29 +19,37 @@ class TestLoad:
             identity = did_web.create_identity()
             claim = crypto.sign_json_claimset(identity, {"foo": "bar"})
             (tmp_path / f"claim{i}.cose").write_bytes(claim)
-        
+
         try:
-            result = subprocess.run([
-                "locust",
-                "-f", "test/load_test/locustfile.py",
-                "--headless",
-                "--skip-log",
-                "--json",
-                "--host", client.url,
-                "--users", "100",
-                "--spawn-rate", "10",
-                "--run-time", "1m",
-                "--scitt-claims", str(tmp_path),],
+            result = subprocess.run(
+                [
+                    "locust",
+                    "-f",
+                    "test/load_test/locustfile.py",
+                    "--headless",
+                    "--skip-log",
+                    "--json",
+                    "--host",
+                    client.url,
+                    "--users",
+                    "100",
+                    "--spawn-rate",
+                    "10",
+                    "--run-time",
+                    "1m",
+                    "--scitt-claims",
+                    str(tmp_path),
+                ],
                 check=True,
                 capture_output=True,
-                text=True)
+                text=True,
+            )
         except subprocess.CalledProcessError as e:
             print(e.stdout)
             print(e.stderr)
             raise
-        
+
         stats = json.loads(result.stdout)
         print(stats)
 
         assert all([s["num_failures"] == 0 for s in stats])
-        
