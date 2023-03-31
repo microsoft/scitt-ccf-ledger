91c4f277011153c89ade0a3a7d4a30cb58b0e355
Kapil Vaswani <kapilv@microsoft.com>
2023-03-31 11:05:07 +0100

    sample contract

--- demo/contract/3-sign-contract.sh
+++ demo/contract/3-sign-contract.sh
@@ -6,18 +6,12 @@ set -ex
 
 TMP_DIR=tmp/$GITHUB_USER
 
-while getopts ":c:" options; do
-    case $options in 
-        c)contract=$OPTARG;;
-    esac
-done
-
-echo "Signing contract $contract..."
+echo "Signing contract..."
 
 CONTENT_TYPE="application/json"
 
 scitt sign-contract \
-    --claims $contract \
+    --claims ./contract.json \
     --content-type "$CONTENT_TYPE" \
     --did-doc $TMP_DIR/did.json \
     --key $TMP_DIR/key.pem \
