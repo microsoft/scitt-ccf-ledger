a8708611b3d641e52e05ee55a26c5d8fa31b529c
Andrea Piccione <and.piccione@gmail.com>
2023-07-18 11:22:41 +0100

    Fix SCITT demo script (#142)

--- demo/github/1-scitt-setup.sh
+++ demo/github/1-scitt-setup.sh
@@ -8,19 +8,20 @@ mkdir -p tmp
 
 SCITT_URL="https://127.0.0.1:8000"
 
-curl -o tmp/cacert.pem "https://ccadb-public.secure.force.com/mozilla/IncludedRootsPEMTxt?TrustBitsInclude=Websites"
+curl -L -o tmp/cacert.pem "https://ccadb-public.secure.force.com/mozilla/IncludedRootsPEMTxt?TrustBitsInclude=Websites"
 scitt governance propose_ca_certs \
+    --name did_web_tls_roots \
     --ca-certs tmp/cacert.pem \
     --url $SCITT_URL \
-    --member-key workspace/sandbox_common/member0_privk.pem \
-    --member-cert workspace/sandbox_common/member0_cert.pem \
+    --member-key workspace/member0_privk.pem \
+    --member-cert workspace/member0_cert.pem \
     --development
 
 echo '{ "authentication": { "allow_unauthenticated": true } }' > tmp/configuration.json
 scitt governance propose_configuration \
     --configuration tmp/configuration.json \
-    --member-key workspace/sandbox_common/member0_privk.pem \
-    --member-cert workspace/sandbox_common/member0_cert.pem \
+    --member-key workspace/member0_privk.pem \
+    --member-cert workspace/member0_cert.pem \
     --development
 
 TRUST_STORE=tmp/trust_store
