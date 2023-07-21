b6c5f020aff8c643bd4405436ba00dd2fd26066c
Andrea Piccione <and.piccione@gmail.com>
2023-07-21 11:20:26 +0000

    Update demo files to support remote instance URL

--- demo/github/1-scitt-setup.sh
+++ demo/github/1-scitt-setup.sh
@@ -6,7 +6,7 @@ set -ex
 
 mkdir -p tmp
 
-SCITT_URL="https://127.0.0.1:8000"
+SCITT_URL=${SCITT_URL:-"https://127.0.0.1:8000"}
 
 curl -L -o tmp/cacert.pem "https://ccadb-public.secure.force.com/mozilla/IncludedRootsPEMTxt?TrustBitsInclude=Websites"
 scitt governance propose_ca_certs \
