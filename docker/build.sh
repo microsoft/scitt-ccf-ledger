9974d44611068f616d06484b7762f7165d718b07
Ivar Prudnikovas <iprudnikovas@microsoft.com>
2023-09-12 14:45:57 +0100

    print mrenclave value after docker build finishes

--- docker/build.sh
+++ docker/build.sh
@@ -28,6 +28,9 @@ DOCKER_BUILDKIT=1 docker build \
     --build-arg SCITT_VERSION_OVERRIDE="$SCITT_VERSION_OVERRIDE" \
     .
 
+echo "MRENCLAVE:"
+docker run --rm -it --entrypoint /bin/cat "$DOCKER_TAG" /usr/src/app/mrenclave.txt
+
 if [ -n "$SAVE_IMAGE_PATH" ]; then
     docker save "$DOCKER_TAG" -o "$SAVE_IMAGE_PATH"
 fi
