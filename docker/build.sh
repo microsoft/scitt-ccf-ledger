541dc3a641fb9f06d4ff86ce5605aeadfd19f4f2
Ivar Prudnikovas <iprudnikovas@microsoft.com>
2023-09-12 15:00:34 +0100

    remove interactive and tty options when inspecting the mrenclave in the image

--- docker/build.sh
+++ docker/build.sh
@@ -29,7 +29,7 @@ DOCKER_BUILDKIT=1 docker build \
     .
 
 echo "MRENCLAVE:"
-docker run --rm -it --entrypoint /bin/cat "$DOCKER_TAG" /usr/src/app/mrenclave.txt
+docker run --rm --entrypoint /bin/cat "$DOCKER_TAG" /usr/src/app/mrenclave.txt
 
 if [ -n "$SAVE_IMAGE_PATH" ]; then
     docker save "$DOCKER_TAG" -o "$SAVE_IMAGE_PATH"
