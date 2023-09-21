bb7eaf509bc986aa1239c907aa3a0e83e01f0e6a
Andrea Piccione <and.piccione@gmail.com>
2023-09-21 14:58:26 +0000

    Implement cose sign for local client

--- pyscitt/pyscitt/local_key_sign_client.py
+++ pyscitt/pyscitt/local_key_sign_client.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+from ccf.cose import create_cose_sign1
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey
@@ -18,7 +19,43 @@ class LocalKeySignClient(MemberAuthenticationMethod):
             password=None,
         )
 
-    def sign(self, data: bytes) -> bytes:
+    def cose_sign(self, data: bytes, cose_headers: dict) -> bytes:
+        """Generates a COSE payload for the specified request with the specified headers.
+
+        https://microsoft.github.io/CCF/main/use_apps/issue_commands.html#signing
+
+        :param data: The intended body for the HTTP request.
+        :type data: bytes
+        :param cose_headers: The headers to include in the COSE payload.
+        :type cose_headers: dict
+
+        :return: The full payload, with signature, to be sent to CCF.
+        :rtype: bytes
+        """
+
+        assert self.cert, "No identity public certificate available for this identity"
+        assert self.key, "No identity private key available for this identity"
+
+        return create_cose_sign1(
+            payload=data,
+            key_priv_pem=self.key,
+            cert_pem=self.cert,
+            additional_protected_header=cose_headers,
+        )
+
+    def http_sign(self, data: bytes):
+        """
+        Generates a HTTP signing payload for the specified request with the specified headers.
+
+        https://microsoft.github.io/CCF/main/use_apps/issue_commands.html#signing
+
+        :param data: The intended body for the HTTP request.
+        :type data: bytes
+
+        :return: The full payload, with signature, to be sent to CCF.
+        :rtype: bytes
+        """
+
         assert isinstance(self.private_key, (EllipticCurvePrivateKey))
         digest_algo = {256: hashes.SHA256(), 384: hashes.SHA384()}[
             self.private_key.curve.key_size
