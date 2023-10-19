2cca9db88fff22141d80b294cd20a5245a93f292
Andrea Piccione <and.piccione@gmail.com>
2023-10-19 16:10:18 +0000

    Fix python script

--- test/infra/generate_cacert.py
+++ test/infra/generate_cacert.py
@@ -1,15 +1,13 @@
 import argparse
+
 from cryptography import x509
 from cryptography.hazmat.primitives import serialization
-from x5chain_certificate_authority import X5ChainCertificateAuthority
 from loguru import logger
 
-def generate_ca_cert_and_key(
-    output_dir: str,
-    alg: str,
-    key_type: str,
-    ec_curve: str    
-):
+from .x5chain_certificate_authority import X5ChainCertificateAuthority
+
+
+def generate_ca_cert_and_key(output_dir: str, alg: str, key_type: str, ec_curve: str):
     # Create a new X5ChainCertificateAuthority instance
     untrusted_ca = X5ChainCertificateAuthority(kty=key_type)
 
@@ -24,6 +22,10 @@ def generate_ca_cert_and_key(
 
     # Write the ca cert to a file
     cert_bundle = b""
+
+    if not identity.x5c:
+        raise ValueError("No x5c field in identity")
+
     for cert in identity.x5c:
         pemcert = x509.load_pem_x509_certificate(cert.encode())
         cert_bundle += pemcert.public_bytes(serialization.Encoding.PEM)
@@ -33,19 +35,34 @@ def generate_ca_cert_and_key(
     with open(output_cacert_file, "wb") as f:
         f.write(cert_bundle)
 
+
 if __name__ == "__main__":
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description="Generate a sample pair of CA cert and associated private key.")
-    parser.add_argument("--output-dir", type=str, help="The directory to output the generated files to.")
-    parser.add_argument("--alg", type=str, help="The algorithm to use to generate the certificate chain.", default="ES256")
-    parser.add_argument("--key-type", type=str, help="The key type to use to generate the certificate chain", default="ec")
-    parser.add_argument("--ec-curve", type=str, help="The Elliptic Curve to use for the key.", default="P-256")
+    parser = argparse.ArgumentParser(
+        description="Generate a sample pair of CA cert and associated private key."
+    )
+    parser.add_argument(
+        "--output-dir", type=str, help="The directory to output the generated files to."
+    )
+    parser.add_argument(
+        "--alg",
+        type=str,
+        help="The algorithm to use to generate the certificate chain.",
+        default="ES256",
+    )
+    parser.add_argument(
+        "--key-type",
+        type=str,
+        help="The key type to use to generate the certificate chain",
+        default="ec",
+    )
+    parser.add_argument(
+        "--ec-curve",
+        type=str,
+        help="The Elliptic Curve to use for the key.",
+        default="P-256",
+    )
     args = parser.parse_args()
 
     # Generate the CA cert and key
-    generate_ca_cert_and_key(
-        args.output_dir,
-        args.alg,
-        args.key_type,
-        args.ec_curve
-    )
+    generate_ca_cert_and_key(args.output_dir, args.alg, args.key_type, args.ec_curve)
