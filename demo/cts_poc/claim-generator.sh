57c69fdd2fb0a68cd356427cdd97a846191ffdaf
Andrea Piccione <and.piccione@gmail.com>
2023-12-22 18:31:48 +0000

    Add small updates to demo scripts

--- demo/cts_poc/claim-generator.sh
+++ demo/cts_poc/claim-generator.sh
@@ -2,17 +2,26 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-### Simple script to generate a signed claim to submit to the SCITT ledger
+### Simple script to generate a signed claim to submit to the SCITT ledger, either using a DID document or a x509 CA certificate.
 
 set -e
 
 # Variables
-: "${CACERT_PATH:?"variable not set. Please define the path to the CA certificate PEM file"}"
 : "${PRIVATE_KEY_PATH:?"variable not set. Please define the path to the private key PEM file"}"
 : "${CLAIM_CONTENT_PATH:?"variable not set. Please define the path to the json/txt file to use as content for the claim"}"
 : "${COSE_CLAIMS_OUTPUT_PATH:?"variable not set. Please define the path where the COSE claim will be saved to"}"
 
 CLAIM_CONTENT_TYPE=${CLAIM_CONTENT_TYPE:-"application/json"}
+
+# Either provide the DID document or the CA certificate to use for signing
+CACERT_PATH=${CACERT_PATH:-""}
+DID_DOC=${DID_DOC:-""}
+
+# Validate that either the DID document or the CA certificate is provided
+if [ -z "$CACERT_PATH" ] && [ -z "$DID_DOC" ]; then
+    echo "Either CACERT_PATH or DID_DOC must be provided"
+    exit 1
+fi
 
 echo -e "\nSetting up environment"
 if [ ! -f "venv/bin/activate" ]; then
@@ -30,6 +39,7 @@ scitt sign \
     --content-type "$CLAIM_CONTENT_TYPE" \
     --key "$PRIVATE_KEY_PATH" \
     --x5c "$CACERT_PATH" \
+    --did-doc "$DID_DOC" \
     --out "$COSE_CLAIMS_OUTPUT_PATH"
 
 echo -e "\nScript completed successfully"
