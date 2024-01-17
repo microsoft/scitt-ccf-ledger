2ddab1d03861168c3a69bfdef412f6da478986e5
Ivar Prudnikovas <iprudnikovas@microsoft.com>
2024-01-17 21:21:18 +0000

    fix inconsistent virtual enclave measurement fallback

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -234,7 +234,7 @@ namespace scitt
         // Node should always get a valid cached measurement on startup
         throw InternalError("Unexpected state - node has no code id");
 #else
-        measurement = "";
+        measurement = "0000000000000000000000000000000000000000000000000000000000000000";
 #endif
       }
 
