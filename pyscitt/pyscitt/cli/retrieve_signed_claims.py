d22216a3994850124c91db08b959fd06bb8aae73
Andrea Piccione <and.piccione@gmail.com>
2024-07-15 16:03:09 +0000

    Rename new cli flag to embed receipts

--- pyscitt/pyscitt/cli/retrieve_signed_claims.py
+++ pyscitt/pyscitt/cli/retrieve_signed_claims.py
@@ -16,7 +16,7 @@ def retrieve_signed_claimsets(
     from_seqno: Optional[int],
     to_seqno: Optional[int],
     service_trust_store_path: Optional[Path],
-    dont_embed_receipt: bool = False,
+    embed_receipt: bool = False,
 ):
     base_path.mkdir(parents=True, exist_ok=True)
 
@@ -26,7 +26,7 @@ def retrieve_signed_claimsets(
         service_trust_store = None
 
     for tx in client.enumerate_claims(start=from_seqno, end=to_seqno):
-        claim = client.get_claim(tx, embed_receipt=(not dont_embed_receipt))
+        claim = client.get_claim(tx, embed_receipt=embed_receipt)
         path = base_path / f"{tx}.cose"
 
         if service_trust_store:
@@ -54,10 +54,10 @@ def cli(fn):
         help="Folder containing JSON parameter files of SCITT services to trust",
     )
     parser.add_argument(
-        "--dont-embed-receipt",
+        "--embed-receipt",
         action="store_true",
         default=False,
-        help="Whether to not return the signed claimsets with the receipt embedded. Default is False.",
+        help="Whether to include the receipt in the signed claimset under the unprotected header. Default is False.",
     )
 
     def cmd(args):
@@ -68,7 +68,7 @@ def cli(fn):
             args.from_seqno,
             args.to_seqno,
             args.service_trust_store,
-            args.dont_embed_receipt,
+            args.embed_receipt,
         )
 
     parser.set_defaults(func=cmd)
