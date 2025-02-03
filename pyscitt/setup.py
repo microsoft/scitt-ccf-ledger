0a4034582195fc01ccd07ea1722241b603861a12
Amaury Chamayou <amchamay@microsoft.com>
2025-02-03 17:19:47 +0000

    16

--- pyscitt/setup.py
+++ pyscitt/setup.py
@@ -25,7 +25,7 @@ setup(
     },
     python_requires=">=3.8",
     install_requires=[
-        "ccf==6.0.0-dev13",
+        "ccf==6.0.0-dev16",
         "cryptography==44.*",  # needs to match ccf
         "httpx",
         "cbor2==5.4.*",
