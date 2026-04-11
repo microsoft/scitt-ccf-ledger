2356a16d67414b04135d0ff603e36a5ccd686c4e
Andrea Piccione <and.piccione@gmail.com>
2026-04-11 01:41:51 +0000

    Update cmakelists and add cpack file

---
+++ app/cpack.cmake
@@ -0,0 +1,7 @@
+include(${CCF_DIR}/cmake/cpack_ccfapp.cmake)
+
+set(CPACK_RPM_PACKAGE_REQUIRES "${CPACK_CCF_RUNTIME_REQUIRES}")
+set(CPACK_RPM_FILE_NAME "scitt")
+set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/scitt")
+
+include(CPack)
