5b0809487e059e992ba04622d10cb65d88714593
Andrea Piccione <and.piccione@gmail.com>
2026-04-11 02:30:58 +0000

    Add missing copyright notice

--- app/cpack.cmake
+++ app/cpack.cmake
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 include(${CCF_DIR}/cmake/cpack_ccfapp.cmake)
 
 set(CPACK_RPM_PACKAGE_REQUIRES "${CPACK_CCF_RUNTIME_REQUIRES}")
