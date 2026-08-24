cae5d3e23489bcf2976cbcf2836045a290c1f5c7
Ivar <iprudnikovas@microsoft.com>
2026-08-24 14:54:04 +0100

    bump pyscitt version

--- pyscitt/setup.py
+++ pyscitt/setup.py
@@ -6,7 +6,7 @@ from os import path
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = "pyscitt"
-PACKAGE_VERSION = "0.14.1"
+PACKAGE_VERSION = "0.14.2"
 
 path_here = path.abspath(path.dirname(__file__))
 
